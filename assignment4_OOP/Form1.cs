8e5ce2e52c63f04ece97bc15b40f62d94ecdc387
Dell <Dell@DESKTOP-TMDV7FV>
2023-01-21 05:47:37 -0800

    Add project files.

---
+++ assignment4_OOP/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace assignment4_OOP
+{
+    public partial class Form1 : Form
+    {
+
+        customer1 customer = new customer1();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            customer.CompanyName = textBox2.Text;
+            customer.ContactName = textBox3.Text;
+            customer.Phone = textBox4.Text;
+            var success = customer.InsertCustomer(customer);
+            dataGridView1.Visible = true;
+            dataGridView1.DataSource = customer.GetEmployees();
+            if (success)
+            {
+                ClearControls();
+                MessageBox.Show("Customer Added Succesfully");
+            }
+            else
+            {
+                MessageBox.Show("Customer Addtion Failed");
+            }
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            customer.CompanyName = textBox2.Text;
+            customer.ContactName = textBox3.Text;
+            customer.Phone = textBox4.Text;
+            customer.CustomerID = textBox1.Text;
+            dataGridView1.Visible = true;
+            var success = customer.UpdateCustomer(customer);
+            dataGridView1.DataSource = customer.GetEmployees();
+            if (success)
+            {
+                ClearControls();
+                MessageBox.Show("Customer Updated Succesfully");
+            }
+            else
+            {
+                MessageBox.Show("Customer Updation Failed");
+            }
+        }
+
+        public void ClearControls()
+        {
+            textBox1.Text = null;
+            textBox2.Text = null;
+            textBox3.Text = null;
+            textBox4.Text = null;
+        }
+
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            customer.CustomerID = textBox1.Text;
+            var success = customer.DeleteCustomer(customer);
+            dataGridView1.Visible = true;
+            dataGridView1.DataSource = customer.GetEmployees();
+            if (success)
+            {
+                ClearControls();
+                MessageBox.Show("Customer Deleted Succesfully");
+            }
+            else
+            {
+                MessageBox.Show("Customer Deletion Failed");
+            }
+
+        }
+
+        private void dataGridView1_RowHeaderMouseDoubleClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            dataGridView1.Visible = true;
+            var index = e.RowIndex;
+            textBox1.Text = dataGridView1.Rows[index].Cells[1].Value.ToString();
+            textBox2.Text = dataGridView1.Rows[index].Cells[2].Value.ToString();
+            textBox3.Text = dataGridView1.Rows[index].Cells[3].Value.ToString();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            ClearControls();
+
+        }
+    }
+}
---
+++ assignment4_OOP/customer1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace assignment4_OOP
+{
+    internal class customer1
+    {
+        private static string mycon = ConfigurationManager.ConnectionStrings["connString"].ConnectionString;
+
+        public string CustomerID { get; set; }
+        public string CompanyName { get; set; }
+        public string ContactName { get; set; }
+        public string Phone { get; set; }
+
+        private const string InsertQuery = "Insert Into customer(CompanyName,ContactName,Phone) Values(@CompanyName, @ContactName, @Phone)";
+
+        //Insert
+        public bool InsertCustomer(customer1 customer)
+        {
+            int rows;
+            using (SqlConnection con = new SqlConnection(mycon))
+            {
+                con.Open();
+                using (SqlCommand com = new SqlCommand(InsertQuery, con))
+                {
+                    com.Parameters.AddWithValue("@CompanyName", customer.CompanyName);
+                    com.Parameters.AddWithValue("@ContactName", customer.ContactName);
+                    com.Parameters.AddWithValue("@Phone", customer.Phone);
+                    rows = com.ExecuteNonQuery();
+                }
+            }
+            return (rows > 0) ? true : false;
+        }
+
+        //Update 
+
+        private const string UpdateQuery = "Update customer set CompanyName=@CompanyName,ContactName=@ContactName,Phone=@Phone where CustomerID=@CustomerID";
+        public bool UpdateCustomer(customer1 customer)
+        {
+            int rows;
+            using (SqlConnection con = new SqlConnection(mycon))
+            {
+                con.Open();
+                using (SqlCommand com = new SqlCommand(UpdateQuery, con))
+                {
+                    com.Parameters.AddWithValue("@CustomerID", customer.CustomerID);
+                    com.Parameters.AddWithValue("@CompanyName", customer.CompanyName);
+                    com.Parameters.AddWithValue("@ContactName", customer.ContactName);
+                    com.Parameters.AddWithValue("@Phone", customer.Phone);
+                    rows = com.ExecuteNonQuery();
+                }
+            }
+            return (rows > 0) ? true : false;
+        }
+
+        //Delete
+
+        private const string DeleteQuery = "Delete from customer where CustomerID=@CustomerID";
+        public bool DeleteCustomer(customer1 customer)
+        {
+            int rows;
+            using (SqlConnection con = new SqlConnection(mycon))
+            {
+                con.Open();
+                using (SqlCommand com = new SqlCommand(DeleteQuery, con))
+                {
+                    com.Parameters.AddWithValue("@CustomerID", customer.CustomerID);
+                    com.Parameters.AddWithValue("@CompanyName", customer.CompanyName);
+                    com.Parameters.AddWithValue("@ContactName", customer.ContactName);
+                    com.Parameters.AddWithValue("@Phone", customer.Phone);
+                    rows = com.ExecuteNonQuery();
+                }
+            }
+            return (rows > 0) ? true : false;
+        }
+
+        //Show
+
+        private const string SelectQuery = "Select * from customer";
+        public DataTable GetEmployees()
+        {
+            var datatable = new DataTable();
+            using (SqlConnection con = new SqlConnection(mycon))
+            {
+                con.Open();
+                using (SqlCommand com = new SqlCommand(SelectQuery, con))
+                {
+                    using (SqlDataAdapter adapter = new SqlDataAdapter(com))
+                    {
+                        adapter.Fill(datatable);
+                    }
+                }
+            }
+            return datatable;
+        }
+    }
+
+
+
+
+}
+
